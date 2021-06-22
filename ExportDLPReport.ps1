03118e882c8bba5bc0ef30ca9d61084e19b925c8
hongphuc9595 <hongphuc9595@gmail.com>
2021-06-22 14:21:18 +0700

    1

---
+++ ExportDLPReport.ps1
@@ -0,0 +1,14 @@
+﻿$report= Get-DlpDetailReport -StartDate 05/01/2021 -EndDate 06/22/2021 -Action BlockAccess |Select  Actor, DLPComplianceRule , DlpCompliancePolicy, SensitiveInformationType, Action , LastModifiedTime, AttachmentNames
+Function Get-FileName($initialDirectory) {   
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+    Out-Null
+
+    $SaveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $SaveFileDialog.initialDirectory = $initialDirectory
+    $SaveFileDialog.filter = "CSV file (*.csv)|*.csv| All Files (*.*)|*.*";
+    $SaveFileDialog.ShowDialog() | Out-Null
+    $SaveFileDialog.filename
+}
+$SaveMyFile = Get-Filename
+
+$report | Export-Csv -Path $SaveMyFile
--- RVADMIN.ps1
+++ RVADMIN.ps1
@@ -1,8 +1,11 @@
 ﻿#Encrypt Password
-#Read-Host -Prompt “Enter your password” -AsSecureString | ConvertFrom-SecureString | Out-File “D:\Password.txt“
+#Read-Host -Prompt “Enter your password” -AsSecureString | ConvertFrom-SecureString | Out-File “D:\OneDrive\HPT\Dự án\Russin&vecchi\PasswordPS.txt“
 
-$AdminName = “rvadm@russinvecchi.com.vn”
-$Pass = Get-Content “D:\Password.txt” | ConvertTo-SecureString
+$AdminName = “nhphuc@russinvecchi.com.vn”
+$Pass = Get-Content “D:\OneDrive\HPT\Dự án\Russin&vecchi\PasswordPS.txt” | ConvertTo-SecureString
 $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminName, $Pass
 Connect-MsolService -Credential $Cred
 Connect-ExchangeOnline -Credential $Cred
+Connect-MsolService -Credential $Cred
+
+#Get-MessageTrace -StartDate (get-date).AddDays(-10) -EndDate (get-date) -SenderAddress *@hpt.vn
